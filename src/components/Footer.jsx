803ccca0945ba0a7ce300f124406a78392ef5804
NARajab <11211067@student.itk.ac.id>
2023-12-14 08:22:32 +0800

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,34 @@
 const Footer = () => {
   return (
-    <div className="grid grid-cols-1 bg-slate-950 px-6 py-10 md:px-10 lg:grid-cols-2">
+    <div className="grid grid-cols-1 px-6 py-10 bg-slate-950 md:px-10 lg:grid-cols-2">
       <div className="md:ml-40">
         <h1 className="text-6xl font-bold text-red-700">Movielist</h1>
       </div>
-      <div className="mt-8 grid grid-cols-2 gap-2 lg:grid-cols-3">
+      <div className="grid grid-cols-2 gap-2 mt-8 lg:grid-cols-3">
         <div className="text-white">
           <h2 className="text-lg font-bold md:text-2xl lg:text-3xl xl:text-4xl ">
             TEAM 5
           </h2>
-          <div className="mt-2 flex flex-col md:mt-4 md:text-2xl xl:text-2xl ">
+          <div className="flex flex-col mt-2 md:mt-4 md:text-2xl xl:text-2xl ">
             <p>Nur Ali Rajab</p>
             <p>Yoga Tiara Wiguna</p>
           </div>
         </div>
+        {/* //wdadawd */}
         <div className="text-white">
           <h2 className="text-lg font-bold md:text-2xl lg:text-3xl xl:text-4xl ">
             RESOURCE
           </h2>
-          <div className="mt-2 flex flex-col md:mt-4 md:text-2xl xl:text-2xl ">
+          <div className="flex flex-col mt-2 md:mt-4 md:text-2xl xl:text-2xl ">
             <p>Tailwinds</p>
             <p>JavaScripts</p>
             <p>React JS</p>
             <p>TMBD API</p>
           </div>
         </div>
-        <div className=" text-white">
-          <h1 className="text-lg font-bold md:text-2xl xl:text-4xl  ">TOOLS</h1>
-          <div className="mt-2 flex flex-col md:mt-4 md:text-lg  xl:text-2xl ">
+        <div className="text-white ">
+          <h1 className="text-lg font-bold md:text-2xl xl:text-4xl ">TOOLS</h1>
+          <div className="flex flex-col mt-2 md:mt-4 md:text-lg xl:text-2xl ">
             <p>Github</p>
             <p>Visual Studio Code</p>
             <p>Postman</p>
